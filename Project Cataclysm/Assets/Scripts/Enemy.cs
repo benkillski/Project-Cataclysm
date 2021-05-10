e00dcfef250e19df649a147a97d10876c486a9e8
Ben Kilkowski <benkilkowski@outlook.com>
2021-05-10 12:47:24 -0500

    Imported NavMeshComponents for Enemy AI

--- Project Cataclysm/Assets/Scripts/Enemy.cs
+++ Project Cataclysm/Assets/Scripts/Enemy.cs
@@ -57,17 +57,62 @@ public class Enemy : MonoBehaviour
 
     private void Patroling()
     {
+        if (!walkPointSet) SearchWalkPoint();
 
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //Walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+    private void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
     }
 
     private void ChasePlayer()
     {
-
+        agent.SetDestination(player.position);
     }
 
     private void AttackPlayer()
     {
+        //Make sure enemy doesnt move
+        agent.SetDestination(transform.position);
 
+        transform.LookAt(player);
+
+        if(!alreadyAttacking)
+        {
+            //Attack code here
+            
+            //
+
+            alreadyAttacking = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacking = false;
     }
 
     private void Die()
